d2fcac07fa9994023ae60d1a5381197c33b92264
Alex Field <38266956+alexfield55@users.noreply.github.com>
2021-03-03 11:58:36 -0700

    Update LMS_p2.sql
    updated based on meeting with teacher and new physical design

--- Phase2/LMS_p2.sql
+++ Phase2/LMS_p2.sql
@@ -1,87 +1,11 @@
-use master;
-drop database if exists LMS
-create database LMS
-go
-use LMS;
-
-create table module
-(
-    moduleID int not null,
-    title varchar(50),
-    name varchar(50),
-    sectionID in not null,
-    constraint prim_moduleID primary key (moduleID),
-    constraint foreign_sectionID foreign key(sectionID) references section(sectionID)
-);
-
-create table final_grade
-(
-    gradeID in not null,
-    letterGrade varchar(2) not null,
-    score decimal not null,
-    sectionID int not null,
-    constraint prim_gradeID primary key (gradeID),
-    constraint foreign_sectionID foreign key(sectionID) references section(sectionID),
-    constraint letterGradeCheck check (letterGrade in ('A+', 'A', 'A-','B+', 'B', 'B-','C+', 'C', 'C-','D+', 'D', 'D-','F', 'E', 'w', 'I')),
-);
-
-create table discussion
-(
-    discussionID int not null,
-    details varchar(50),
-    sectionID int not null,
-    constraint prim_discussionID primary key (discussionID),
-    constraint foreign_sectionID foreign key(sectionID) references section(sectionID)
-);
-
-create table reply
-(
-    replyID int not null,
-    usernameTeacher varchar(50),
-    usernameStudent varchar(50) --this seems like it should connect to student
-    discussionID int not null,
-    constraint prim_replyID primary key (replyID),
-    constraint foreign_discussionID foreign key (discussionID) references dicussion(discussionID),
-    constraint foreign_usernameTeacher foreign key (usernameTeacher) references teacher(username),
-    constraint foreign_usernameStudent foreign key (usernameStudent) references student(username)
-);
-
-create table home
-(
-    homeID int not null,
-    pageShown varchar(50),
-    sectionID int not null,
-    constraint prim_homeID primary key (homeID),
-    constraint foreign_sectionID foreign key(sectionID) references section(sectionID)
-);
-
 create table section
 (
     sectionID int not null,
-    roomID int,
     teacherID int not null,
     classID int not null,
     constraint prim_sectionID primary key (sectionID),
-    constraint foreign_roomID foreign key (roomID) references room(roomID),
     constraint foreign_teacherID foreign key (teacherID) references teacher(teacherID),
     constraint foreign_classID foreign key (classID) references class(classID)
-);
-
-create table room
-(
-    roomID int not null,
-    roomType varchar(50),
-    buildingID int not null,
-    constraint prim_roomID primary key (roomID),
-    constraint foreign_buildingID foreign key (buildingID) references building(buildingID)
-);
-
-create table building
-(
-    buildingID int not null,
-    location varchar(50) not null,
-    name varchar(50) not null
-    constraint prim_buildingID primary key buildingID
 );
 
 create table assignment
@@ -90,7 +14,7 @@ create table assignment
 	assignmentID int not null,
 	description varchar(50),
 	due_date date not null, 
-	max_possible_grade float,
+	max_possible_grade decimal,
 	submission_type varchar(30),
     constraint foreign_sectionID foreign key(sectionID) references section(sectionID)
 	constraint submission_names check (submission_type in ('Text Entry', 'Media Recording', 'File Upload', 'Website URL')),
@@ -104,9 +28,14 @@ create table student_section
 (
     sectionID int not null,
     studentID int not null,
+    letterGrade varchar(2) not null,
+    score decimal not null,
     constraint composite_prim_key primary key (sectionID, studentID),
     constraint foreign_sectionID foreign key(sectionID) references section(sectionID),
-    constraint foreign_studentID foreign key(studentID) references student(studentID)
+    constraint foreign_studentID foreign key(studentID) references student(studentID),
+	constraint max_sagrade check (score <= 100),
+	constraint min_sagrade check (score >= 0),
+    constraint letterGradeCheck check (letterGrade in ('A+', 'A', 'A-','B+', 'B', 'B-','C+', 'C', 'C-','D+', 'D', 'D-','F', 'E', 'w', 'I')),
 );
 
 create table teacher
@@ -132,6 +61,7 @@ create table student
 	email varchar(50),
 	majorID int, --this is not normal
     minorID int, --this is not normal
+    tuituion decimal,
 	constraint unique_email unique (email),
 	constraint prim_studentID primary key (studentID),
     constraint foreign_minor foreign key (minorID) references department(deptID),
@@ -146,8 +76,7 @@ create table class
     prerequisites int not null, --this is not normalized
     deptID int not null,
     constraint prim_classID primary key (classID),
-    constraint foreign_deptID foreign key (deptID) references department(deptID)
-    
+    constraint foreign_deptID foreign key (deptID) references department(deptID)  
 );
 
 create table department
@@ -155,20 +84,6 @@ create table department
     deptID int not null,
     name varchar(10) not null, --changed shortname to this
     description varchar(50) not null,
-);
-
-create table student_assignment
-(
-	assignmentID int not null,
-	studentID int not null,
-	submission_date date,
-	grade float,
-	constraint max_sagrade check (grade <= 100),
-	constraint min_sagrade check (grade >= 0),
-	constraint comp_key primary key (assignment_id, studentID),
-	constraint foreign1_sa foreign key(assignmentID) references assignment(assignmentID),
-	constraint foreign2_sa foreign key(studentID) references student(studentID)
-
 );
 
 create table announcements
