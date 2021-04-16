d7134e681f80712644581dea196b19ad4a7c2795
Dax Allen <dax.e.allen@gmail.com>
2021-04-16 14:18:59 -0600

    Finished 4

--- Phase3/part1.sql
+++ Phase3/part1.sql
@@ -15,11 +15,20 @@ WHERE student.studentID = 10 AND student_section.semester = 'Spring 2020' -- CHA
 -- 3
 SELECT assignment.description, assignment_student.grade FROM assignment
 INNER JOIN student_section ON student_section.sectionID = assignment.sectionID
-INNER JOIN 
+INNER JOIN assignment_student ON assignment_student.studentID = student_section.studentID
+INNER JOIN section ON section.sectionID = student_section.sectionID
+INNER JOIN class ON class.classID = section.classID
+WHERE student_section.studentID = 5 AND class.classID = 2
 
 -- 4
+SELECT student.fname, student.lname from student
+INNER JOIN student_section ON student_section.studentID = student.studentID
+INNER JOIN section ON section.sectionID = student_section.sectionID
+INNER JOIN class ON class.classID = section.classID
+WHERE class.classID = 2
 
 -- 5
+
 
 -- 6
 
